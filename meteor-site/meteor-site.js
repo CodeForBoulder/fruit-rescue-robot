4744dec47fd14a44148f951fa818d51670812698
Brandon Minor <gallimatrix@gmail.com>
2015-09-23 20:18:17 -0600

    Added global variables according to model fields

--- meteor-site/meteor-site.js
+++ meteor-site/meteor-site.js
@@ -1,23 +1,36 @@
+// All of our global variables
+
+// Types of humans...
+HumansList = new Mongo.Collection('humans');
+HarvestersList = new Mongo.Collection('harvesters');
+AdminsList = new Mongo.Collection('admins');
+CoordinatorsList = new Mongo.Collection('coordinators');
+
+PropertiesList = new Mongo.Collection('properties');
+TreesList = new Mongo.Collection('trees');
+HumanPropertiesList = new Mongo.Collection('human_properties');
+HarvestsList = new Mongo.Collection('harvests');
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+		// counter starts at 0
+		Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+		Template.hello.helpers({
+				counter: function () {
+						return Session.get('counter');
+				}
+		});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+		Template.hello.events({
+				'click button': function () {
+						// increment the counter when button is clicked
+						Session.set('counter', Session.get('counter') + 1);
+				}
+		});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+		Meteor.startup(function () {
+				// code to run on server at startup
+		});
 }
