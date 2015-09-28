9f558bbd24f164d472bc66ac3029e9f490de505a
bminortx <gallimatrix@gmail.com>
2015-09-28 14:45:41 -0600

    Added iron:router to dependencies

--- meteor-site/dependencies.sh
+++ meteor-site/dependencies.sh
@@ -3,5 +3,8 @@
 meteor add mquandalle:jade
 meteor add coffeescript
 meteor add less
-meteor add accounts-ui accounts-password 
+meteor add accounts-ui accounts-password
+meteor add accounts-ui-unstyled
+meteor add iron:router
+
 
